9e73c086a67f10838ed2b57a666a64f67a540074
bossones <Bogdan_04>
2020-11-16 19:58:07 +0300

    Deleted all

--- testing/src/main/java/net/bossones/Base.java
+++
@@ -1,5 +0,0 @@
-package net.bossones;
-
-public final class Base {
-    public final static String BASE = "http://localhost:80/mock";
-}
--- testing/src/test/java/net/bossones/AppTest.java
+++
@@ -1,9 +0,0 @@
-package net.bossones;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-
-}
